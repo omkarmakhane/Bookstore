3a0c90b6793e1509400d5f7fcbaea3d790d30dd9
omkar makhane <omkarmakhne@gmail.com>
2024-09-12 00:42:18 +0530

    Backed Done !!!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import Home from './home/Home'
 
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Courses from './courses/Courses'
 import Contacts from './contacts/Contacts'
 import Abouts from './components/About'
 import Signup from "./components/Signup"
-
-
+import { Toaster } from 'react-hot-toast';
+import {useAuth} from "./context/AuthProvider"
 function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
+  
   return (
     <>
    <div className='dark:bg-slate-900 dark:text-white'>
    <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Course" element={<Courses />} />
+      <Route 
+      path="/Course" element={authUser ? <Courses /> : <Navigate to="/signup" />} 
+      />
       <Route path="/Contact" element={<Contacts />} />
       <Route path="/About" element={<Abouts />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
-
+    <Toaster />
    </div>
     </>
   )
---
+++ Frontend/src/components/Logout.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useAuth } from '../context/AuthProvider'
+import toast from 'react-hot-toast';
+
+function Logout() {
+    const [authUser, setAuthUser]=useAuth();
+    const handleLogout=()=>{
+        try {
+            setAuthUser({
+                ...authUser,
+                user:null
+            })
+            localStorage.removeItem("Users")
+            toast.success("Logout successfull");
+            window.location.reload();
+        } catch (error) {
+            toast.error("Error: " + error.message)
+        }
+    }
+  return (
+    <div>
+        <button className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'>
+        Logout
+        </button>
+    </div>
+  )
+}
+
+export default Logout
